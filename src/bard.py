e3cc95570b4f366ae9929414707a567b3c9741df
Armand Halbert <armand.halbert@gmail.com>
2014-03-02 23:56:57 -0500

    Changed bard.py to meet the interface spec.

--- src/bard.py
+++ src/bard.py
@@ -3,10 +3,8 @@ import time
 import random
 import copy
 from SimpleXMLRPCServer import SimpleXMLRPCServer 
+import xmlrpclib
 
-global tally
-tally ={"Gaul":(0,0,0), "Rome":(0,0,0)}
-global score 
 global tally_exit
 tally_exit = False
 global score_exit
@@ -23,98 +21,56 @@ class Bard(threading.Thread):
             self.changeMedalTally()
         if self.mode == "score":
             self.updateScore()
-        else:
-            self.server()
-
-    def server(self):
-        server = SimpleXMLRPCServer(("localhost", 8001))
-        print "Server running"
-        server.register_function(get_tally, "get_tally")
-        server.register_function(getMedalTally, "getMedalTally")
-        server.register_function(incrementMedalTally, "incrementMedalTally")
-        server.register_function(get_score, "getScore")
-        server.register_function(set_score, "setScore")
-        server.serve_forever()
-
 
     def updateScore(self):
-            global score
-            print "score running"
-            score = (0,0)
-            while not score_exit: 
-                time.sleep(5)
-                if random.random() >= .95:
-                    team = random.randrange(1,3)
-                    if team == 1:
-                        score = (score[0]+1,score[1])
-                    else:
-                        score = (score[0],score[1]+1)
+        score = (0,0,False)
+        should_end == False
+        while not score_exit:
+            team, sport = get_team(), get_sport()
+            if random.random >= .99:
+                should_end == True
+            if team == "Romans":
+                proxy.setScore(sport, (score[1]+1, score[2], should_end))
+            else:
+                proxy.setScore(sport, (score[1], score[2]+1, should_end))
 
-    def changeMedalTally(self):
-        print "tally running"
-        while not tally_exit: 
+    def updateTally(self):
+        while not tally_exit:
             time.sleep(5)
-            lock.acquire()
-            if random.random() >= .75:
-                medal_type = random.randrange(0,3)
-                team = random.randrange(1,3)
-                incrementMedalTally(team, medal_type)
-            lock.release()
+            if random.random >= .75:
+                proxy.incrementMedalTaly(get_team(), get_medal_type())
 
-def get_score(sport):
-    #lock.acquire()
-    s = copy.copy(score)
-    #lock.release()
-    return s
-
-def set_score(sport, sc):
-    score = sc
-    return 5
-
-def get_tally():
-    #lock.acquire()
-    t = copy.copy(tally)
-    #lock.release()
-    return t
-
-def getMedalTally(team):
-    t = copy.copy(tally)
-    return t[team]
-
-def incrementMedalTally(team, medal_type):
-    if team == "Gaul":
-        team = 1
+def get_medal_type():
+    num = random.randrange(1,4)
+    if num == 1:
+        return "Gold"
+    if num == 2:
+        return "Silver"
     else:
-        team = 0
-    if medal_type == "Gold":
-        medal_type == 0
-    elif medal_type == "Silver":
-        medal_type == 1
-    else:
-        medal_type == 2
-    if team == 1:
-            if (medal_type == 0):
-                tally["Gaul"] = (tally["Gaul"][0]+1, tally["Gaul"][1], tally["Gaul"][2])
-            if (medal_type == 1):
-                tally["Gaul"] = (tally["Gaul"][0], tally["Gaul"][1]+1, tally["Gaul"][2])
-            if (medal_type == 2):
-                tally["Gaul"] = (tally["Gaul"][0], tally["Gaul"][1], tally["Gaul"][2]+1)
-    else:
-            if (medal_type == 0):
-                tally["Rome"] = (tally["Rome"][0]+1, tally["Rome"][1], tally["Rome"][2])
-            if (medal_type == 1):
-                tally["Rome"] = (tally["Rome"][0], tally["Rome"][1]+1, tally["Rome"][2])
-            if (medal_type == 2):
-                tally["Rome"] = (tally["Rome"][0], tally["Rome"][1], tally["Rome"][2]+1)
-    return 5
+        return "Bronze"
 
-lock = threading.Lock()
+
+def get_sport():
+    num = random.randrange(1,4)
+    if num == 1:
+        return "Curling"
+    if num == 2:
+        return "Skating"
+    else:
+        return "Skiing"
+
+def get_team():
+    num = random.randrange(1,3)
+    if num == 1:
+        return "Gauls"
+    else:
+        return "Romans"
+
+proxy = xmlrpclib.ServerProxy("http://localhost:8080") 
 tally_thread = Bard("tally")
 tally_thread.start()
 score_thread = Bard("score")
 score_thread.start()
-server_thread = Bard("server")
-server_thread.start()
 try:
     while True:
         time.sleep(1)
